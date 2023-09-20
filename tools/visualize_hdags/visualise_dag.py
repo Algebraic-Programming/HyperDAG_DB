6e46bf897d6853aef95e7b4e67cd26f4e4996b5f
Benjamin Lozes <benjamin.lozes@protonmail.com>
2023-09-20 14:27:49 +0200

    DAG visulisation tool refactoring

--- tools/visualize_hdags/visualise_dag.py
+++ tools/visualize_hdags/visualise_dag.py
@@ -5,7 +5,7 @@ import scipy as sp
 import networkx as nx
 import matplotlib.pyplot as plt
 import functools
-from typing import Tuple, List
+from typing import Tuple, List, Any
 
 
 def _read_matrix_market(filename: str) -> nx.DiGraph:
@@ -24,7 +24,7 @@ def _read_matrix_market(filename: str) -> nx.DiGraph:
             pass
         # Read the information of the matrix: nrows, ncols, nnz
         nnz = int(current_line.split()[2])
-        print(f"Reading {nnz} edges...")
+        print(f"Reading {nnz} edges from {filename} ...")
         # Read the data
         for _ in range(nnz):
             current_line = fp.readline()
@@ -34,49 +34,65 @@ def _read_matrix_market(filename: str) -> nx.DiGraph:
             G.add_edge(row, col, weight=val)
         return G
 
+
 def _find_sink_nodes(G: nx.DiGraph) -> list:
     return [node for node in G.nodes if G.out_degree(node) == 0]
 
-def _font_weight_sink_nodes(G: nx.DiGraph, sink_nodes: list, not_sink="normal", sink="bold") -> List[str]:
+
+def _font_weight_sink_nodes(
+    G: nx.DiGraph, sink_nodes: list, not_sink="normal", sink="bold"
+) -> List[str]:
     # Weight sink nodes
-    weights = [ not_sink if sink_nodes.count(node) == 0 else sink for node in G.nodes ]
+    weights = [not_sink if sink_nodes.count(node) == 0 else sink for node in G.nodes]
     return weights
 
-def _node_color_sink_nodes(G: nx.DiGraph, sink_nodes: list, not_sink="k", sink="r") -> List[str]:
+
+def _node_color_sink_nodes(
+    G: nx.DiGraph, sink_nodes: list, not_sink="k", sink="r"
+) -> List[str]:
     # Colorise sink nodes
-    colours = [ not_sink if sink_nodes.count(node) == 0 else sink for node in G.nodes ]
+    colours = [not_sink if sink_nodes.count(node) == 0 else sink for node in G.nodes]
     return colours
 
-def _lerp(minimum, maximum, value):
-    return minimum + (maximum - minimum) * value
+
+def _lerp(minimum, maximum, value) -> Any:
+    result = minimum + (maximum - minimum) * value
+    return min(maximum, max(minimum, result))
+
 
 @functools.lru_cache
 def _get_weights(G: nx.DiGraph) -> Tuple[float, float, List[float]]:
-    weights = [ edge[2]["weight"] for edge in G.edges(data=True) ]
-    return (
-        min(weights),
-        max(weights),
-        weights
-    )
+    weights = [edge[2]["weight"] for edge in G.edges(data=True)]
+    return (min(weights), max(weights), weights)
+
 
 def _arrowsize_by_weight(G: nx.DiGraph, min_size=10, max_size=20) -> List[float]:
     min_w, max_w, edges_weights = _get_weights(G)
     if min_w == max_w:
-        return [ min_size for _ in edges_weights ]
-    return [ _lerp(min_size, max_size, (w - min_w) / (max_w - min_w)) for w in edges_weights ]
+        return [min_size for _ in edges_weights]
+    return [
+        _lerp(min_size, max_size, (w - min_w) / (max_w - min_w)) for w in edges_weights
+    ]
+
 
 def _edgewidth_by_weight(G: nx.DiGraph, min_width=0.5, max_width=3) -> list:
     min_w, max_w, edges_weights = _get_weights(G)
     if min_w == max_w:
-        return [ min_width for _ in edges_weights ]
-    return [ _lerp(min_width, max_width, (w - min_w) / (max_w - min_w)) for w in edges_weights ]
+        return [min_width for _ in edges_weights]
+    return [
+        _lerp(min_width, max_width, (w - min_w) / (max_w - min_w))
+        for w in edges_weights
+    ]
+
 
 def command(filename) -> None:
     G = _read_matrix_market(filename)
+    print(f"Searching for sink nodes ...")
     sink_nodes = _find_sink_nodes(G)
+    print(f"Generating the graph rendering ...")
     nx.draw(
         G,
-       # pos=nx.kamada_kawai_layout(G),
+        pos=nx.kamada_kawai_layout(G),
         with_labels=True,
         arrows=True,
         node_size=200,
@@ -85,10 +101,13 @@ def command(filename) -> None:
         width=_edgewidth_by_weight(G),
         font_color="white",
         edge_color="gray",
-        node_color=_node_color_sink_nodes(G, sink_nodes)
+        node_color=_node_color_sink_nodes(G, sink_nodes),
     )
     # Save to file named filename + '.png' (1920x1080)
-    plt.savefig(filename + ".png", dpi=312)
+    print(f"Saving to {filename}.png ...")
+    plt.savefig(
+        filename + ".png", dpi=312, format="png", bbox_inches="tight", transparent=True
+    )
 
 
 if __name__ == "__main__":
