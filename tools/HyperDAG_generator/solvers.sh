c9713de8fee2537a61b2de21884affb642bc9364
Benjamin Lozes <benjamin.lozes@protonmail.com>
2023-10-27 10:21:30 +0200

    solver.sh improvement

--- tools/HyperDAG_generator/solvers.sh
+++ tools/HyperDAG_generator/solvers.sh
@@ -1,10 +1,18 @@
+if [ $# -lt 4 ]; then
+    echo "Usage: $0 <executable> <A> <L> <U>"
+    exit 1
+fi
+
 executable = $1
 A=$2
 L=$3
 U=$4
 
-$1 spmv -sink -reindex -output spmv_$A -inputs $A
-$1 lltsolver -sink -reindex -output lltsolver_$A -inputs $L
-$1 alltsolver -sink -reindex -output alltsolver_$A -inputs $A $L
-$1 lusolver -sink -reindex -output lusolver_$A -inputs $L $U
-$1 alusolver -sink -reindex -output alusolver_$A -inputs $A $L $U
+INPUT_FILENAME=$(basename "$A")
+DIRNAME=$(dirname "$A")
+
+$1 spmv -sink -reindex -output $DIRNAME/spmv_$INPUT_FILENAME -inputs $A
+$1 lltsolver -sink -reindex -output $DIRNAME/lltsolver_$INPUT_FILENAME -inputs $L
+$1 alltsolver -sink -reindex -output $DIRNAME/alltsolver_$INPUT_FILENAME -inputs $A $L
+$1 lusolver -sink -reindex -output $DIRNAME/lusolver_$INPUT_FILENAME -inputs $L $U
+$1 alusolver -sink -reindex -output $DIRNAME/alusolver_$INPUT_FILENAME -inputs $A $L $U
